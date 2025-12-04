049ab683f966ac9d01beb0f4ab2817702372f61b
Vicente Scópise <vscopise@gmail.com>
2025-12-04 15:49:56 -0300

    4/12

--- src/app/components/listado-parlanchines.tsx
+++ src/app/components/listado-parlanchines.tsx
@@ -14,23 +14,23 @@ export const ListadoParlanchines = () => {
   if (parlanchines.length === 0) return null;
 
   return (
-    <div className="p-2 mt-2 bg-amber-50 w-3/4 text-center rounded-xl border border-amber-500">
-      <p className="font-bold text-amber-500">Parlanchines:</p>
-      {parlanchines.map((p, index) => {
-        return (
+    <div className="flex-1 overflow-y-auto">
+      <p className="font-bold text-amber-500 text-center">Parlanchines:</p>
+      <div className="flex w-full flex-wrap justify-center gap-2  bg-amber-50 rounded-xl border border-amber-500 p-2">
+        {parlanchines.map((p, index) => (
           <div
             key={index}
-            className={`cursor-pointer ${
-                p.intervenciones > 1 
-                ? "text-red-500 font-bold text-xl"
-                : "text-gray-800"
+            className={`inline-flex rounded-md py-0.5 px-2.5 border border-transparent cursor-pointer text-white ${
+              p.intervenciones > 1
+                ? "font-bold text-xl bg-red-500"
+                : " bg-gray-400"
             }`}
             onClick={() => delParlanchin(p.nombre)}
           >
-            {p.nombre} ({p.intervenciones})
+            {p.nombre} {p.intervenciones >= 2 && `(${p.intervenciones})`}
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 };
